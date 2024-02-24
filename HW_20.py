162ed34123f31c05599ec128600fd74678b24cd6
bunturioto <Bunturioto@gmail.com>
2024-02-24 15:24:25 +0400

    let see where going insert data left or right subtree

--- HW_20.py
+++ HW_20.py
@@ -22,3 +22,35 @@ class BinaryTree:
             # recursively insert the data
             self._insert(self.root, data)
 
+    def _insert(self, data, current_node):
+        # Compare the data to be inserted with the data of the current node
+        if data < current_node.data:
+            # If the data is less than the current node's data,
+            # go to the left subtree
+            if current_node.left is None:
+                # If the left child is None, create a new node with the data
+                # and set it as the left child of the current node
+                current_node.left = Node(data)
+            else:
+                # If the left child is not None, recursively call _insert
+                # with the left child as the new current node
+                self._insert(data, current_node.left)
+
+        elif data > current_node.data:
+            # If the data is greater than the current node's data,
+            # go to the right subtree
+            if current_node.right is None:
+                # If the right child is None, create a new node with the data
+                # and set it as the right child of the current node
+                current_node.right = Node(data)
+            else:
+                # If the right child is not None, recursively call _insert
+                # with the right child as the new current node
+                self._insert(data, current_node.right)
+        else:
+            # If the data is equal to the current node's data,
+            # it's a duplicate value
+            print("Value already in tree!")
+
+
+
