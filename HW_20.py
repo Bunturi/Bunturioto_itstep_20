f8178f2fb534f4ee671d62e99a334f3d5d12a5cc
bunturioto <Bunturioto@gmail.com>
2024-02-24 15:06:58 +0400

    Insertion Method, chek if tree is empty, if not insert data

--- HW_20.py
+++ HW_20.py
@@ -5,3 +5,20 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
+
+
+# Define the Binary Tree Class
+class BinaryTree:
+    def __init__(self, root):
+        self.root = Node(root)
+
+    #  Implement Insertion Method
+    def insert(self, data):
+        # Check if the tree is empty
+        if self.root is None:
+            # create a new node as the root
+            self.root = Node(data)
+        else:
+            # recursively insert the data
+            self._insert(self.root, data)
+
